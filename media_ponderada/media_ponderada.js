6c8c43657e1ff92f021387ecd8d07eeca48d4510
Marcelo <marcelocavalcantef@gmail.comgit config --global user.email marcelocavalcantef@gmail.comgit config --global user.email marcelocavalcantef@gmail.com>
2023-09-05 11:47:51 -0300

    alteracoes

--- media_ponderada/media_ponderada.js
+++ media_ponderada/media_ponderada.js
@@ -2,5 +2,5 @@ var nota1 = parseFloat(prompt("Digite nota 1: "));
 var nota2 = parseFloat(prompt("Digite nota 2: "));
 var nota3 = parseFloat(prompt("Digite nota 3: "));
 
-media = (nota1*2 + nota2*3 + nota3*5 / 10)
+media = ((nota1*2 + nota2*3 + nota3*5) / 10)
 console.log(media)
